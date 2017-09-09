75cb6b777d771b4dcd5ae0c50926d882937fd419
Aaron <aaron.schriever@gmail.com>
2017-09-09 10:11:13 +0930

    added props for equation handling

--- calculator-app/src/App.js
+++ calculator-app/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { display: 0 };
+    this.state = { display: 0, equationLHS: 0, equationRHS: 0 };
     this.changeDisplay=this.changeDisplay.bind(this);
   }
 
@@ -31,34 +31,34 @@ class NumberPad extends React.Component {
     return (
       <div className="number-pad">
         <div className="buttonRow">
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="MC" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="+/-" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="<-" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="C" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="MC" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="+/-" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="<-" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="C" />
         </div>
         <div className="buttonRow">
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="7" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="8" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="9" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="&divide;" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="7" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="8" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="9" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="&divide;" />
         </div>
         <div className="buttonRow">
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="4" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="5" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="6" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="&times;" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="4" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="5" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="6" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="&times;" />
         </div>
         <div className="buttonRow">
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="1" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="2" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="3" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="-" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="1" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="2" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="3" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="-" />
         </div>
         <div className="buttonRow">
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="0" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="." />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="=" />
-          <CalcButton display={this.props.display} changeDisplay={this.props.changeDisplay} character="+" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="0" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="." />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="=" />
+          <CalcButton equationLHS={this.props.equationLHS} display={this.props.display} changeDisplay={this.props.changeDisplay} character="+" />
         </div>
       </div>
     );
@@ -75,18 +75,33 @@ function CalcDisplay(props) {
 
 class CalcButton extends React.Component {
   handleDisplayInput(e) {
-    let display = this.props.display + this.props.character;
-    if (this.props.character === "C") {
-      display = '0';
+    let display = this.props.display;
+    switch (this.props.character) {
+      case 'C':
+        display = '0';
+        break;
+      case '<-':
+        display = this.props.display.substring(0, this.props.display.length-1);
+        break;
+      case '0':
+      if (this.props.display.length > 1){
+        console.log(this.props.display.length);
+        display = this.props.display + this.props.character;
+      } else{
+        display = this.props.character;
+      }
+        
+        break;
+      default:
+        display = this.props.display + this.props.character;
+      
+      }
+
+      this.props.changeDisplay(display);
     }
-    if (this.props.character === "<-") {
-      display = this.props.display.substring(0, this.props.display.length-1);
-    }
-    if (this.props.display === '0'){
-      display = this.props.character;
-    }
-    this.props.changeDisplay(display);
-  }
+    
+    
+  
   render(){
   return (
     
